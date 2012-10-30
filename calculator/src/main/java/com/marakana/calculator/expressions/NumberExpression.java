493d2d996b791ccf27473aa6030370981b18b555
Dan Rosen <dan@marakana.com>
2012-10-30 10:23:32 -0700

    number and operation are value classes, should not have other extraneous behavior...

--- calculator/src/main/java/com/marakana/calculator/expressions/NumberExpression.java
+++ calculator/src/main/java/com/marakana/calculator/expressions/NumberExpression.java
@@ -1,11 +1,40 @@
 package com.marakana.calculator.expressions;
 
 public class NumberExpression implements Expression {
+
 	private final int number;
 
 	public NumberExpression(int number) {
 		this.number = number;
 	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + number;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NumberExpression other = (NumberExpression) obj;
+		if (number != other.number)
+			return false;
+		return true;
+	}
+
+	// TODO: move this stuff out
 
 	@Override
 	public int evaluate() {
--- calculator/src/main/java/com/marakana/calculator/expressions/OperationExpression.java
+++ calculator/src/main/java/com/marakana/calculator/expressions/OperationExpression.java
@@ -3,6 +3,7 @@ package com.marakana.calculator.expressions;
 import com.marakana.calculator.Operator;
 
 public class OperationExpression implements Expression {
+
 	private final Operator op;
 	private final Expression lhs, rhs;
 
@@ -11,6 +12,48 @@ public class OperationExpression implements Expression {
 		this.lhs = lhs;
 		this.rhs = rhs;
 	}
+
+	public Operator getOp() {
+		return op;
+	}
+
+	public Expression getLhs() {
+		return lhs;
+	}
+
+	public Expression getRhs() {
+		return rhs;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + lhs.hashCode();
+		result = prime * result + op.hashCode();
+		result = prime * result + rhs.hashCode();
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OperationExpression other = (OperationExpression) obj;
+		if (!lhs.equals(other.lhs))
+			return false;
+		if (op != other.op)
+			return false;
+		if (!rhs.equals(other.rhs))
+			return false;
+		return true;
+	}
+	
+	// TODO: move this stuff out
 
 	@Override
 	public int evaluate() {
